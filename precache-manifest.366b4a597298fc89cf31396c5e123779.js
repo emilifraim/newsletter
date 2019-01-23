8a652f802903268c625e34f64a6b60ffe283cd4a
Emil Ifraimov <emilifraim@gmail.com>
2019-01-23 13:07:45 -0500

    Updates

---
+++ precache-manifest.366b4a597298fc89cf31396c5e123779.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c827e6a6edcce8347fff",
+    "url": "/newsletter/static/js/runtime~main.c827e6a6.js"
+  },
+  {
+    "revision": "b44a9d4c82507b1ed552",
+    "url": "/newsletter/static/js/main.b44a9d4c.chunk.js"
+  },
+  {
+    "revision": "b825c73f7c6e707f577b",
+    "url": "/newsletter/static/js/1.b825c73f.chunk.js"
+  },
+  {
+    "revision": "b44a9d4c82507b1ed552",
+    "url": "/newsletter/static/css/main.18edc3ff.chunk.css"
+  },
+  {
+    "revision": "d6097ecd27acefa149970b2d318427a9",
+    "url": "/newsletter/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/newsletter/precache-manifest.98ac20efc7d0d64e1324072df3f6158a.js"
+  "/newsletter/precache-manifest.366b4a597298fc89cf31396c5e123779.js"
 );
 
 workbox.clientsClaim();
